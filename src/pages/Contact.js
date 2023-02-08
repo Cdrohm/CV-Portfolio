87350e8e63629175601371a84855a43c6b7647d6
Clément Théraud <theraud.clement@gmail.com>
2023-02-08 21:51:52 +0100

    try deploy

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from '../components/Nav';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+//import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Contact = () => {
     return (
